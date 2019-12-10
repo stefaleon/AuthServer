5ee3c8fbcc3a1bd0cb0393af08d10c3ff32519d2
stefaleon <stefaleon@gmail.com>
2019-12-10 20:38:28 +0200

    postSignUp - sign up user with email and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,5 +5,28 @@ exports.getSignUp = (req, res, next) => {
 };
 
 exports.postSignUp = (req, res, next) => {
-  res.send({ "response from": "postSignUp" });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use" });
+    }
+
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+      res.json({ "sign-up-success": `Added user with email ${user.email}` });
+    });
+  });
 };
